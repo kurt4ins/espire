08edac197969131d25e9955eb90c2ad7cd035105
romanenko_vova <bonyshd@gmail.com>
2024-07-22 11:17:51 +0300

    fv 3

--- espire/settings.py
+++ espire/settings.py
@@ -144,8 +144,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = (BASE_DIR / "static",)
-# STATICFILES_ROOT = BASE_DIR / "static/"
-# STATIC_ROOT = BASE_DIR / "static/"
+
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / "static"
+
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media/"
