07e851c98ea92ea939a2121e98a943b2506fbb4d
romanenko_vova <bonyshd@gmail.com>
2024-07-22 10:36:55 +0300

    fv

--- firstsite/settings.py
+++ firstsite/settings.py
@@ -190,7 +190,10 @@ INTERNAL_IPS = [
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": "redis://127.0.0.1:6379/1", 
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
     }
 }
 
